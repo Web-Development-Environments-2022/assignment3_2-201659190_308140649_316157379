40db14183c5fd9c5aabfe7d52e03c6bccf404f08
Shahar <Shaharhadad123@gmail.com>
2022-06-11 18:50:26 +0300

    update package and add .env parameters

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,8 @@ router.post("/Register", async (req, res, next) => {
       parseInt(process.env.bcrypt_saltRounds)
     );
     await DButils.execQuery(
-      `INSERT INTO users VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
+      `INSERT INTO users (userName, firstName, lastName, country, password, email)
+      VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
       '${user_details.country}', '${hash_password}', '${user_details.email}')`
     );
     res.status(201).send({ message: "user created", success: true });
