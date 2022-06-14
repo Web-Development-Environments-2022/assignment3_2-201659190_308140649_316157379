d89ee98ebd843de6f9316da96be6f0e4eecdeb8b
Shahar <Shaharhadad123@gmail.com>
2022-06-14 20:30:02 +0300

    update family function

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,12 +1,23 @@
 const DButils = require("./DButils");
 
+
+async function getRecipeFromDB(column_name, id){
+    let recipe = await DButils.execQuery(`select '${column_name}' from FavoriteRecipes where user_id='${id}'`);
+    return recipe;
+}
+
 async function markAsFavorite(user_id, recipe_id){
     await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
 }
 
 async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
-    return recipes_id;
+    const fav_recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
+    return fav_recipes_id;
+}
+
+async function getFamilyRecipes(user_id){
+    const family_recipes_id = await DButils.execQuery(`select recipe_id from familyrecipes where user_id='${user_id}'`);
+    return family_recipes_id;
 }
 
 async function get_new_recipe_id(user_id) {
@@ -15,7 +26,8 @@ async function get_new_recipe_id(user_id) {
 }
 
 
-
+exports.getRecipeFromDB = getRecipeFromDB;
+exports.getFamilyRecipes = getFamilyRecipes;
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
 exports.get_new_recipe_id = get_new_recipe_id;
